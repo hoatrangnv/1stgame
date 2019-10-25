d210bc6eb36337572855bab8446922e9b1ed8ee1
kadiesnguyen <kadiesnguyen@gmail.com>
2019-10-25 21:53:30 +0700

    Game

---
+++ Assets/00Uwin/GateGame/Scripts/EventVipPoint/TabEventRewardLevel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabEventRewardLevel : BaseTabEventVip {
+
+    public List<UIItemEventRewardVipPoint> listItemReward;
+    public GameObject txtBigReceive;
+    public override void Init()
+    {
+        base.Init();
+        for (int i = 0; i < listItemReward.Count; i++)
+        {
+            listItemReward[i].Init(i);
+        }
+    }
+
+    private void OnWebServiceResponse(WebServiceCode.Code code, WebServiceStatus.Status status, string data)
+    {
+        switch (code)
+        {
+            case WebServiceCode.Code.ReceiveLevelVipPoint:
+                MAccountVipPoint mAccountVipPointResponse = JsonUtility.FromJson<MAccountVipPoint>(data);
+                if (mAccountVipPointResponse.ResponseStatus == -1)
+                {
+                    ((LPopup)UILayerController.Instance.ShowLayer(UILayerKey.LPopupTop)).ShowPopup("THÔNG BÁO", "Lỗi server", "", "Hủy Bỏ");
+                    return;
+                }
+                Database.Instance.SetAccountVipPointInfo(mAccountVipPointResponse);
+                
+                Init();
+                break;
+            
+        }
+    }
+    public override void Show()
+    {
+        base.Show();
+        WebServiceController.Instance.OnWebServiceResponse += OnWebServiceResponse;
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+        WebServiceController.Instance.OnWebServiceResponse -= OnWebServiceResponse;
+    }
+}
---
+++ Assets/00Uwin/GateGame/Scripts/EventVipPoint/UIItemEventRewardVipPoint.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItemEventRewardVipPoint : MonoBehaviour {
+
+    public Image imgIcon;
+    public Text txtPoint;
+    public Text txtReward;
+    public ButtonGray buttonReceive;
+    public GameObject boxUnlockReceive;
+    MVipPointDatabase vipPointData;
+    private int level;
+    public void Init(int level)
+    {
+        this.level = level;
+        vipPointData = DatabaseServer.ListVipPointDatabasee[level];
+        txtPoint.text = VKCommon.ConvertStringMoney(vipPointData.VipPoint);
+        txtReward.text = VKCommon.ConvertStringMoney(vipPointData.RewardLevel);
+
+        if (vipPointData.RewardLevel <= 0)
+        {
+            buttonReceive.gameObject.SetActive(false);
+            boxUnlockReceive.gameObject.SetActive(true);
+        }
+        else
+        {
+            buttonReceive.gameObject.SetActive(true);
+            boxUnlockReceive.gameObject.SetActive(false);
+            InitBtnReceiveState();
+        }
+    }
+
+    private void InitBtnReceiveState()
+    {
+        MAccountVipPoint accountVip = Database.Instance.AccountVipPoint();
+        if (accountVip.LevelMax > 0 && level <= accountVip.LevelMax - 1)
+        {
+            string[] levelRewardList = accountVip.LevelReward.Split(',');
+            for (int i = 1; i < levelRewardList.Length; i++)
+            {
+                if (level == int.Parse(levelRewardList[i]))
+                {
+                    buttonReceive.gameObject.SetActive(false);
+                    boxUnlockReceive.gameObject.SetActive(true);
+                    return;
+                }
+            }
+            buttonReceive.IsActive = true;
+        }
+        else
+        {
+            buttonReceive.IsActive = false;
+        }
+       
+    }
+
+    public void OnBtnReceive()
+    {
+        MAccountVipPoint mAccountVipPoint = Database.Instance.AccountVipPoint();
+        SendRequest.SendReceiveLevelVipPoint(level);
+    }
+}
